6ee5bc4a9abcf84b651ca202864c7d2a3fbcdaae
fanta-git <fantaji4957t@gmail.com>
2023-03-09 23:58:12 +0900

    整形 fetchPlaylistにdefaultExportを追加

--- src/foundations/api/fetchPlaylist/index.ts
+++ src/foundations/api/fetchPlaylist/index.ts
@@ -1,1 +1,8 @@
 export * from './kiite'
+import { PlaylistTypes } from '@/types/playlist'
+import * as fetchPlaylistItems from '.'
+
+export default async function fetchPlaylist(type: PlaylistTypes, id: string) {
+    const playlist = await fetchPlaylistItems[type](id)
+    return playlist
+}
