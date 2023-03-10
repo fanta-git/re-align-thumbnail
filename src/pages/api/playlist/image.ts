ccc36fafa4e5c1b550c2cc5e71a68a65161b5082
fanta-git <fantaji4957t@gmail.com>
2023-03-10 14:31:47 +0900

    追加 API側のエラーハンドリング

--- src/pages/api/playlist/image.ts
+++ src/pages/api/playlist/image.ts
@@ -3,20 +3,26 @@ import fetchPlaylist from "@/foundations/api/fetchPlaylist";
 import fetchThumbnails from "@/foundations/api/fetchThumbnails";
 import { getPlaylistQuery } from "@/foundations/api/getQuery";
 import { NextApiRequest, NextApiResponse } from "next";
-import * as fs from "fs"
 
 export default async function image(req: NextApiRequest, res: NextApiResponse) {
-    const query = getPlaylistQuery(req)
-    if (query === undefined) return res.status(400).send({ error: 'invalid query' })
+    try {
+        const query = getPlaylistQuery(req)
+        if (query === undefined) return res.status(400).json({ error: 'invalid query' })
 
-    const { type, id } = query
-    const songs = await fetchPlaylist(type, id)
-    if (songs === undefined) return res.status(400).send({ error: 'faild fetch playlist' })
-    const thumbnails = await fetchThumbnails(songs) as string[]
-    const alignedSharp = await align(thumbnails)
-    const buffer = alignedSharp.jpeg().toBuffer()
-
-    res.status(200)
-        .setHeader("content-Type", 'image/jpeg')
-        .send(buffer)
+        const { type, id } = query
+        const songs = await fetchPlaylist(type, id)
+        if (songs === undefined) return res.status(400).json({ error: 'faild fetch playlist' })
+        const thumbnails = await fetchThumbnails(songs) as string[]
+        const alignedSharp = await align(thumbnails)
+        const buffer = await alignedSharp.jpeg().toBuffer()
+        res.status(200)
+        res.setHeader("content-Type", 'image/jpeg')
+        res.send(buffer)
+    } catch (e) {
+        if (e instanceof Error) {
+            res.status(400).json({ error: e.message })
+        }
+    } finally {
+        res.end()
+    }
 }
