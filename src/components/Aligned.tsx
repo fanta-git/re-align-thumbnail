120c226dd69911a7e5f04e47ccb23d76ad3d5c07
fanta-git <fantaji4957t@gmail.com>
2023-03-10 14:39:55 +0900

    追加 Webページでの画像の表示

--- src/components/Aligned.tsx
+++ src/components/Aligned.tsx
@@ -1,10 +1,16 @@
 import { currentPlaylistBaseState } from "@/stores/playlist"
+import Image from "next/image"
 import { useRecoilValue } from "recoil"
 
 export default function Aligned() {
   const playlist = useRecoilValue(currentPlaylistBaseState)
+  if (playlist === undefined) return <>please input playlist url</>
+  const imgUrl = `/api/playlist/image?type=${playlist.type}&id=${playlist.id}`
 
   return (
-    <>{JSON.stringify(playlist)}</>
+    <>
+      {imgUrl}
+      <Image width={1600} height={900} alt={""} src={imgUrl} />
+    </>
   )
 }
