19e0e02f480ca48d302878cbba46df3fa433b0a7
fanta-git <fantaji4957t@gmail.com>
2023-04-18 11:33:11 +0900

    変更 SizeFormを一つのファイルに統合

--- src/components/SizeForm.tsx
+++ src/components/SizeForm.tsx
@@ -2,23 +2,18 @@ import { sizeFormHeads, sizeFormItemData } from "@/consts/form";
 import fixFixedValues from "@/foundations/fixFixedValues";
 import { FormContents, SizeFormLabels } from "@/types/form";
 import { createObject } from "@/util/arrays";
-import { RadioGroup, Table, TableContainer, Th, Thead, Tr } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightAddon, Radio, RadioGroup, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
-import { SizeFormItem } from "./SizeFormItem";
 
 const sizeFormItems = sizeFormItemData.flatMap(v => v.item);
 const targets = Object.keys(fixFixedValues) as (keyof typeof fixFixedValues)[]
 
 export function SizeForm () {
-    const [fixed, setFixed] = useState<SizeFormLabels>()
+    const [fixed, setFixed] = useState<SizeFormLabels>(sizeFormItemData.at(-1)!.label)
     const { register, watch, setValue } = useFormContext<FormContents>()
     const watchFields = watch(targets)
     const oldField = useRef<string[]>([])
-
-    useEffect(() => {
-        setFixed(sizeFormItemData.at(-1)!.label)
-    }, [])
 
     useEffect(() => {
         const changedIndex = watchFields.findIndex((v, i) => v !== oldField.current[i])
@@ -50,7 +45,23 @@ export function SizeForm () {
                             {sizeFormHeads.map((v, i) => (<Th key={i}>{v}</Th>))}
                         </Tr>
                     </Thead>
-                    {sizeFormItemData.map((data, i) => (<SizeFormItem key={i} {...{data, fixed, register}} />))}
+                    {sizeFormItemData.map((data, i) => (
+                        <Tbody key={i}>
+                            <Tr>
+                                <Td>
+                                    <Radio value={data.label}>{data.label}</Radio>
+                                </Td>
+                                {data.item.map(item => (
+                                    <Td key={item.register}>
+                                        <InputGroup>
+                                            <Input {...item.inputProps} disabled={fixed === data.label} {...register(item.register)} />
+                                            <InputRightAddon>{item.prefix}</InputRightAddon>
+                                        </InputGroup>
+                                    </Td>
+                                ))}
+                            </Tr>
+                        </Tbody>
+                    ))}
                 </Table>
             </TableContainer>
         </RadioGroup>
