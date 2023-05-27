c71bab4962debe38c885bf825746a0247806d712
fanta-git <fantaji4957t@gmail.com>
2023-05-27 17:47:21 +0900

    サムネイルが縦長になってもある程度正しく表示できるように

--- src/hooks/useAlign.ts
+++ src/hooks/useAlign.ts
@@ -5,6 +5,9 @@ import { canvas2URL, createCanvas } from "@/util/canvas";
 import { getImage } from "@/util/image";
 import { useState } from "react";
 
+const RATIO_W = 16
+const RATIO_H = 9
+
 export default function useAlign () {
     const [aligned, setAligned] = useState<string>()
     const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -29,10 +32,15 @@ export default function useAlign () {
                     const y = Math.round(i * outputHeight / rows)
                     const tnWidth = Math.round((j + 1) * outputWidth / columns) - x
                     const tnHeight = Math.round((i + 1) * outputHeight / rows) - y
-                    const flameWidth = Math.min(imageHeight * tnWidth / tnHeight | 0, imageWidth)
-                    const flameHeight = Math.min(imageWidth * tnHeight / tnWidth | 0, imageHeight)
+
+                    const trimWidth = Math.min(imageHeight * RATIO_W / RATIO_H | 0, imageWidth)
+                    const trimHeight = Math.min(imageWidth * RATIO_H / RATIO_W | 0, imageHeight)
+
+                    const flameWidth = Math.min(trimHeight * tnWidth / tnHeight | 0, trimWidth)
+                    const flameHeight = Math.min(trimWidth * tnHeight / tnWidth | 0, trimHeight)
                     const flameX = (imageWidth - flameWidth) / 2 | 0
                     const flameY = (imageHeight - flameHeight) / 2 | 0
+
                     context.drawImage(image, flameX, flameY, flameWidth, flameHeight, x, y, tnWidth, tnHeight)
                 } catch (e) {
                     console.error(e)
