527a90c19db0f78e5db544bdeae3b566bdac16d0
fanta-git <fantaji4957t@gmail.com>
2023-05-27 12:21:17 +0900

    サムネイル画像をキャッシュするように

--- src/util/image.ts
+++ src/util/image.ts
@@ -1,6 +1,17 @@
+const cache = new Map<string, HTMLImageElement>()
+
 export const getImage = async (url: string) => new Promise<HTMLImageElement>((resolve, reject) => {
+    const cached = cache.get(url)
+    if (cached) {
+        resolve(cached)
+        return
+    }
+
     const image = new Image()
-    image.addEventListener('load', () => resolve(image))
+    image.addEventListener('load', () => {
+        cache.set(url, image)
+        resolve(image)
+    })
     image.addEventListener('error', e => reject(e))
     image.src = url
 })
