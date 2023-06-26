db2712204eab59118f1c1467abf685a6fcf601c0
fanta-git <fantaji4957t@gmail.com>
2023-06-26 12:49:06 +0900

    生成した画像のキャッシュをしないように

--- src/components/AlignField.tsx
+++ src/components/AlignField.tsx
@@ -1,7 +1,7 @@
 import align from "@/foundations/align";
 import { optionFormContentsState, playlistContentsState, sizeFormContentsState } from "@/stores/playlist";
 import { Loadable } from "@/util/Loadable";
-import { Suspense, useMemo } from "react";
+import { Suspense } from "react";
 import { useRecoilValue } from "recoil";
 import AlignedImage from "./AlignedImage";
 
@@ -9,7 +9,7 @@ export default function AlignField () {
   const playlist = useRecoilValue(playlistContentsState)
   const size = useRecoilValue(sizeFormContentsState)
   const option = useRecoilValue(optionFormContentsState)
-  const loadableUrl = useMemo(() => playlist && new Loadable(align(playlist, size, option)), [playlist, size, option])
+  const loadableUrl = playlist && new Loadable(align(playlist, size, option))
 
   if (loadableUrl === undefined) return <>Error!</>
 
--- src/components/AlignedImage.tsx
+++ src/components/AlignedImage.tsx
@@ -1,5 +1,6 @@
 import { Loadable } from "@/util/Loadable";
 import Image from "next/image";
+import { useEffect } from "react";
 
 type Props = {
   loadableUrl: Loadable<string | undefined>
@@ -8,8 +9,12 @@ type Props = {
 };
 export default function AlignedImage (props: Props) {
   const { loadableUrl, width, height } = props
-
   const url = loadableUrl.load()
+
+  useEffect(() => () => {
+    if (url) URL.revokeObjectURL(url)
+  }, [url])
+
   if (url == null) return (<>Error!</>)
 
   return <Image src={url} alt={""} width={width} height={height} />
