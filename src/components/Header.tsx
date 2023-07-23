5dbca451e8d4b520436790644b8595ec8a718380
fanta-git <fantaji4957t@gmail.com>
2023-07-24 00:07:08 +0900

    ヘッダーにヘルプとGitHubのボタンを追加（ハリボテ）

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,44 @@
-import { Box, Container, Flex, Heading } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Container, HStack, Heading, Icon, Spacer, Center, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { IconType } from "react-icons";
+import { IoLogoGithub, IoMdHelpCircleOutline } from 'react-icons/Io';
 
 export default function Header() {
   return (
     <Box color={"brand.50"} bgColor={"brand.900"}>
       <Container maxW="container.lg">
-        <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
-          <Link href="/">
-            <Heading as='h1' fontSize="2xl" cursor="pointer">
-              Re:AlignThumbnail
-            </Heading>
+        <HStack spacing={[5, 10]}>
+          <Box py="4">
+            <NextLink href="/">
+              <Heading as='h1' fontSize="2xl" cursor="pointer">
+                Re:AlignThumbnail
+              </Heading>
+            </NextLink>
+          </Box>
+          <Spacer />
+          <Link as={NextLink} href={"/"} _hover={{ color: "brand.400" }}>
+            <LabelOrIcon label={"使い方"} icon={IoMdHelpCircleOutline} />
           </Link>
-        </Flex>
+          <Link as={NextLink} href={"/"} _hover={{ color: "brand.400" }}>
+            <LabelOrIcon label={"GitHub"} icon={IoLogoGithub} />
+          </Link>
+        </HStack>
       </Container>
-  </Box>
+    </Box>
+  )
+}
+
+function LabelOrIcon(props: { icon: IconType, label: string }) {
+  const { icon, label } = props
+
+  return (
+    <>
+      <Center display={["block", "none"]}>
+        <Icon as={icon} boxSize={8} />
+      </Center>
+      <Box display={["none", "block"]} fontWeight={"bold"}>
+        {label}
+      </Box>
+    </>
   )
 }
