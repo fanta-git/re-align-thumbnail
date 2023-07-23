94d41fd8a2fd9a7381628261c9d8a39a38d1144d
fanta-git <fantaji4957t@gmail.com>
2023-07-23 19:01:38 +0900

    ヘッダーの装飾

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <Heading>Re:AlignThumbnail</Heading>
+    <Box color={"brand.50"} bgColor={"brand.900"}>
+      <Container maxW="container.lg">
+        <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
+          <Link href="/">
+            <Heading as='h1' fontSize="2xl" cursor="pointer">
+              Re:AlignThumbnail
+            </Heading>
+          </Link>
+        </Flex>
+      </Container>
+  </Box>
   )
 }
